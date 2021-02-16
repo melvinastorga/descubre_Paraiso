3f34126d2328a4a34fc02252ea148b894d8cb6e3
Melissa <meli8538@gmail.com>
2021-02-16 14:50:29 -0600

    Update in BD files

---
+++ bd/InsertTablas.sql
@@ -0,0 +1,33 @@
+
+Insert into TipoAtractivo(
+nombreTA)
+values
+("Restaurante"),
+("Naturaleza"),
+("Recreacion");
+
+Insert into Edad(
+nombreEdad) 
+values
+("Menor de 17 años"),
+("entre 18 y 40 años"),
+("40 años en adelante"); 
+
+Insert into Precio(
+nombrePrecio) 
+values
+("de 0 a 25 000 colónes"),
+("entre 25 000 a 50 000 colónes"),
+("50 000 colónes en adealante");
+
+Insert into TipoTurista(
+nombreTT) values 
+("Nacional"),
+("Extranjero"),
+("Ambos");
+
+Insert into Ubicacion (nombreUbicacion)
+values 
+("Paraiso"), 
+("Cachi"), 
+("Orosi");
---
+++ bd/Tablas.sql
@@ -0,0 +1,52 @@
+Create table TipoAtractivo (
+idTA int auto_increment primary key, 
+nombreTA varchar(40)
+);
+
+Create table Edad(
+idE int auto_increment primary key, 
+rangoEdad varchar(40)
+);
+
+Create table Precio(
+idP int auto_increment primary key, 
+nombrePrecio varchar(40)
+);
+
+Create table TipoTurista(
+idTT int auto_increment primary key, 
+nombreTT varchar(40)
+);
+
+Create table Ubicacion (
+idU int auto_increment primary key, 
+nombreUbicacion varchar(40)
+);
+
+Create table Clasificacion(
+idC int auto_increment primary key, 
+nombreClasificacion varchar(40)
+);
+
+Create table Sitio(
+idS int auto_increment primary key, 
+nombreSitio varchar(60),
+descripcion varchar(500), 
+latitud decimal(15, 14), 
+latitud decimal(15, 14), 
+tipoA int,
+edad int,
+precio int, 
+tipoTurista int, 
+ubicacion int, 
+clasificacion int,  
+foreign key (tipoA) references TipoAtractivo(idTA), 
+foreign key (edad) references Edad(idE), 
+foreign key (precio) references Precio(idP), 
+foreign key (tipoTurista) references TipoTurista(idTT), 
+foreign key (ubicacion) references Ubicacion(idU), 
+foreign key (clasificacion) references Clasificacion(idC)
+);
+
+
+

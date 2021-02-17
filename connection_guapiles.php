4947da8b0ecff39ef7d32dda9e4dc9b25b3b1357
Melvin Astorga <melvin.astorga@gmail.com>
2021-02-16 18:53:18 -0600

    conexion bd guapiles

---
+++ connection_guapiles.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "163.178.107.2";
+$dbname = "descrubre_paraiso_bd";
+$user = "laboratorios";
+$password = "Saucr.120A";
+
+$connection = mysqli_connect($host,$user,$password,$dbname) or die("No se pudo conectar a la BD: ".mysqli_connect_error());
+
+mysqli_set_charset($connection,"utf8");
+
+
+
+?>
